d454d8fc19396e8e7755e455834f9e01191b4808
Jan <j.c.scherch@gmail.com>
2024-01-20 10:03:32 +0100

    users.js in data added :)

---
+++ src/data/users.js
@@ -0,0 +1,191 @@
+export const users = [
+  {
+    id: 1,
+    userName: "daria",
+    fName: "Daria",
+    lName: "Morgendorffer",
+    email: "daria.morgendorffer@gmail.com",
+    password: "daria",
+    profilePicture: "../images/daria.gif",
+    about:
+      "Daria is an intelligent and sarcastic teenage girl known for her deadpan wit and cynical outlook on life. She lives in the fictional town of Lawndale with her well-meaning but clueless family. Daria is often portrayed as an outsider and a social critic, navigating the challenges of high school with her best friend Jane Lane. Despite her aloof demeanor, Daria is a complex character who grapples with issues of identity, friendship, and the absurdities of the world around her. The character resonated with audiences for her sharp observations and unconventional approach to societal norms.",
+  },
+  {
+    id: 2,
+    userName: "eleven",
+    fName: "Jane",
+    lName: "Ives",
+    email: "eleven@gmail.com",
+    password: "eleven",
+    profilePicture: "../images/eleven.gif",
+    about:
+      "Eleven is a young girl with psychokinetic abilities who escapes from a secretive government laboratory. She encounters a group of kids in the town of Hawkins, forming a close bond with them. Eleven is characterized by her shaved head and distinctive telekinetic powers, which she often uses to protect her friends and confront the supernatural threats in Hawkins. As the series progresses, Eleven embarks on a journey of self-discovery, uncovering her mysterious past and the origins of her extraordinary abilities.",
+  },
+  {
+    id: 3,
+    userName: "fry",
+    fName: "Philip",
+    lName: "Fry",
+    email: "philip.fry@gmail.com",
+    password: "fry",
+    profilePicture: "../images/fry.gif",
+    about:
+      "He is a delivery boy from the 20th century who, after accidentally freezing himself on December 31, 1999, wakes up 1,000 years in the future. Fry becomes an employee at Planet Express, a delivery company, where he interacts with a diverse group of characters, including his distant nephew, the one-eyed Leela, and the alcoholic robot Bender. Despite being initially portrayed as lazy and lacking ambition, Fry develops into a lovable, good-natured character known for his loyalty and moments of surprising wisdom. ",
+  },
+  {
+    id: 4,
+    userName: "fry",
+    fName: "Philip",
+    lName: "Fry",
+    email: "philip.fry@gmail.com",
+    password: "fry",
+    profilePicture: "../images/fry.gif",
+    about:
+      "He is a delivery boy from the 20th century who, after accidentally freezing himself on December 31, 1999, wakes up 1,000 years in the future. Fry becomes an employee at Planet Express, a delivery company, where he interacts with a diverse group of characters, including his distant nephew, the one-eyed Leela, and the alcoholic robot Bender. Despite being initially portrayed as lazy and lacking ambition, Fry develops into a lovable, good-natured character known for his loyalty and moments of surprising wisdom. ",
+  },
+
+  {
+    id: 5,
+    userName: "garfield",
+    fName: "Garfield",
+    lName: "Arbuckle",
+    email: "garfield.@gmail.com",
+    password: "garfield",
+    profilePicture: "../images/garfield.gif",
+    about:
+      "Garfield is a fat, lazy, and cynical cat who loves to eat, especially lasagna. He resides with his owner, Jon Arbuckle, and his fellow pet, Odie the dog. Garfield spends most of his time sleeping, eating, and avoiding any form of exertion. His sarcastic and sardonic humor, combined with his love for food, has endeared him to audiences worldwide. Despite his grumpy demeanor, Garfield has a soft spot for his owner, Jon, and occasionally engages in moments of unexpected kindness. The comic strip and animated series revolving around Garfield's antics have become widely popular, making him one of the most iconic and enduring characters in the world of comics and animation.",
+  },
+  {
+    id: 6,
+    userName: "jerry",
+    fName: "Jerry",
+    lName: "Mouse",
+    email: "jerry.mouse@gmail.com",
+    password: "jerry",
+    profilePicture: "../images/jerry.gif",
+    about:
+      "Jerry is a small brown house mouse who is the arch-nemesis and constant adversary of Tom, the cat. Despite his diminutive size, Jerry is clever, resourceful, and quick-witted. He often outsmarts Tom in their ongoing comedic battles, using his intelligence and agility to evade the cat's attempts to catch him. Jerry is known for his iconic high-pitched laugh and his ability to turn ordinary household items into tools for his escape. The dynamic and entertaining rivalry between Tom and Jerry has made them one of the most beloved and enduring duos in the history of animation.",
+  },
+  {
+    id: 7,
+    userName: "kirby",
+    fName: "Kirby",
+    lName: "Nintendo",
+    email: "kirby@gmail.com",
+    password: "kirby",
+    profilePicture: "../images/kirby.gif",
+    about:
+      "Kirby is a lovable pink puffball known for his ability to inhale enemies and gain their powers. Hailing from the planet Pop Star, Kirby embarks on adventures to save his home from various threats. Despite his innocent appearance, Kirby is a powerful and brave hero, always ready to protect his friends and the universe.",
+  },
+
+  {
+    id: 8,
+    userName: "mrbean",
+    fName: "Mr.",
+    lName: "Bean",
+    email: "mrbean@gmail.com",
+    password: "mrbean",
+    profilePicture: "../images/mrbean.gif",
+    about:
+      "Mr. Bean, played by Rowan Atkinson, is a bumbling and eccentric character known for his comedic misadventures. With a knack for finding himself in unusual situations, Mr. Bean relies on his wit and resourcefulness to navigate through life's challenges, often causing chaos and laughter wherever he goes.",
+  },
+  {
+    id: 9,
+    userName: "patrick",
+    fName: "Patrick",
+    lName: "Star",
+    email: "patrick.star@gmail.com",
+    password: "patrick",
+    profilePicture: "../images/patrick.gif",
+    about:
+      "Patrick Star is SpongeBob SquarePants' best friend and a friendly, but somewhat dim-witted, starfish. Living under a rock in Bikini Bottom, Patrick enjoys simple pleasures and often provides comedic relief. Despite his lack of intelligence, Patrick's heart is always in the right place, making him an endearing character in the world of SpongeBob.",
+  },
+  {
+    id: 10,
+    userName: "peter",
+    fName: "Peter",
+    lName: "Griffin",
+    email: "peter.griffin@gmail.com",
+    password: "peter",
+    profilePicture: "../images/peter.gif",
+    about:
+      "Peter Griffin is the lovable but often clueless protagonist of the animated series 'Family Guy.' Residing in the fictional town of Quahog, Peter is known for his offbeat humor and absurd adventures with his quirky family. Despite his flaws, Peter's genuine love for his family and friends shines through in unexpected moments.",
+  },
+  {
+    id: 11,
+    userName: "pikachu",
+    fName: "Pikachu",
+    lName: "Ketchum",
+    email: "pikachu@gmail.com",
+    password: "pikachu",
+    profilePicture: "../images/pikachu.gif",
+    about:
+      "Pikachu is an iconic Electric-type Pokémon known for its cute appearance and lightning powers. As the mascot of the Pokémon franchise, Pikachu travels with Pokémon Trainers, including Ash Ketchum, on exciting journeys. Pikachu's electrifying abilities and adorable charm have made it a beloved character among Pokémon fans.",
+  },
+  {
+    id: 12,
+    userName: "ron",
+    fName: "Ron",
+    lName: "Weasley",
+    email: "ron.weasley@gmail.com",
+    password: "ron",
+    profilePicture: "../images/ron.gif",
+    about:
+      "Ron Weasley is a loyal and brave wizard, known for his friendship with Harry Potter and Hermione Granger. Growing up in a large wizarding family, Ron faces various challenges at Hogwarts School of Witchcraft and Wizardry. Despite his humorous and sometimes clumsy nature, Ron plays a crucial role in the fight against dark forces in the wizarding world.",
+  },
+  {
+    id: 13,
+    userName: "sailormoon",
+    fName: "Usagi",
+    lName: "Tsukino",
+    email: "usagi.tsukino@gmail.com",
+    password: "sailormoon",
+    profilePicture: "../images/sailormoon.gif",
+    about:
+      "Usagi Tsukino, also known as Sailor Moon, is the titular character in the magical girl anime and manga series. A cheerful and kind-hearted teenager, Usagi transforms into Sailor Moon to protect the world from evil forces. Alongside her Sailor Senshi friends, Usagi embarks on a mission to defend love and justice.",
+  },
+  {
+    id: 14,
+    userName: "salem",
+    fName: "Salem",
+    lName: "Saberhagen",
+    email: "salem@gmail.com",
+    password: "salem",
+    profilePicture: "../images/salem.gif",
+    about:
+      "Salem Saberhagen is a talking cat and a magical companion in the TV series 'Sabrina the Teenage Witch.' Once a powerful witch, Salem is cursed to spend centuries as a cat for attempting world domination. Despite his mischievous personality, Salem provides humor and wisdom to Sabrina Spellman, the teenage witch.",
+  },
+  {
+    id: 15,
+    userName: "shaggy",
+    fName: "Shaggy",
+    lName: "Rogers",
+    email: "shaggy.rogers@gmail.com",
+    password: "shaggy",
+    profilePicture: "../images/shaggy.gif",
+    about:
+      "Shaggy Rogers is a laid-back and perpetually hungry character in the 'Scooby-Doo' franchise. As a member of Mystery Inc., Shaggy, along with his Great Dane Scooby-Doo, investigates supernatural mysteries. Despite his fear of ghosts and monsters, Shaggy's love for food and his easygoing nature make him a beloved and iconic member of the team.",
+  },
+  {
+    id: 16,
+    userName: "squidward",
+    fName: "Squidward",
+    lName: "Tentacles",
+    email: "squidward.tentacles@gmail.com",
+    password: "squidward",
+    profilePicture: "../images/squidward.gif",
+    about:
+      "Squidward Tentacles is a grumpy and cynical octopus in the animated series 'SpongeBob SquarePants.' Working at the Krusty Krab alongside SpongeBob and Mr. Krabs, Squidward often finds himself annoyed by the cheerful antics of his coworkers. Despite his irritable nature, Squidward possesses artistic talents and dreams of a more sophisticated and peaceful life.",
+  },
+  {
+    id: 17,
+    userName: "winniepooh",
+    fName: "Winnie",
+    lName: "the Pooh",
+    email: "winnie.pooh@gmail.com",
+    password: "winniepooh",
+    profilePicture: "../images/winniepooh.gif",
+    about:
+      "Winnie the Pooh, often simply called Pooh, is a lovable and honey-loving bear who resides in the Hundred Acre Wood. Created by A.A. Milne, Pooh is the central character in various stories and animated adaptations. Known for his gentle nature, love of simple pleasures, and friendship with characters like Piglet, Eeyore, and Tigger, Winnie the Pooh embodies the spirit of childhood and the joy of companionship.",
+  },
+];
