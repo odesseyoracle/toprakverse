17644bf2cbfc5357f5b599cee535c7e1b8d34553
mellacid <melisa.bcd@icloud.com>
2024-01-22 15:04:29 +0100

    navigation added and router installed

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function App() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/SignUp">Sign Up</Link>
+        </li>
+        <li>
+          <Link to="/UserProfile">Profil</Link>
+        </li>
+        <li>
+          <Link to="/Friends">Friends</Link>
+        </li>
+        <li>
+          <Link to="/Login">Log Out</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation; 
