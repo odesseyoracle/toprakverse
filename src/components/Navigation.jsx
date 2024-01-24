ad4ce2b573e74a638d207ea28c9eb1e3a7a7d70f
mellacid <melisa.bcd@icloud.com>
2024-01-24 15:12:17 +0100

    nav component and toggle nav

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+function Navigation() {
+  const [isNavOpen, setNavOpen] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
+
+  const toggleNav = () => {
+    setNavOpen(!isNavOpen);
+    setIsClicked(true);
+
+   
+    setTimeout(() => {
+      setIsClicked(false);
+    }, 200);
+  };
+
+  return (
+    <nav>
+      {/* Burger Menu Icon */}
+      <div
+        onClick={toggleNav}
+        className={`burger-menu-icon ${isClicked ? "active" : ""}`}
+        style={{ transform: isClicked ? "scale(1.1)" : "scale(1)", transition: "transform 0.2s ease-in-out" }}
+      >
+        <i className="fa-solid fa-burger"></i>
+      </div>
+
+      {/* Navigation Links */}
+      <ul className={`nav-list ${isNavOpen ? "block" : "hidden"}`}>
+        <li>
+          <Link to="/toprakverse" className="block">Home</Link>
+        </li>
+        <li>
+          <Link to="/signUp" className="block">Sign Up</Link>
+        </li>
+        <li>
+          <Link to="/userProfile" className="block">Profil</Link>
+        </li>
+        <li>
+          <Link to="/friends" className="block">Friends</Link>
+        </li>
+        <li>
+          <Link to="/login" className="block">Log Out</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
+
