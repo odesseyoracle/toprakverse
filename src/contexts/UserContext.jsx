86286db7dccc10c04d65d6c785ce0f25f476b08a
Jan <j.c.scherch@gmail.com>
2024-01-25 11:44:13 +0100

    test

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -32,6 +32,7 @@ const userDataReducer = (state, action) => {
 export default function UserContextProvider({ children }) {
   const [userData, dispatch] = useReducer(userDataReducer, users);
   const [loggedUserId, setLoggedUserId] = useState(0);
+  // Test
 
   return (
     <UserContext.Provider
