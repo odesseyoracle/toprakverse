54ed5fd8ea34424f9aa8b0754d26e078bf3d6ee8
dci-student <lailabeqiri@libero.it>
2024-01-29 11:07:12 +0100

    Profile picture style

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,7 +4,7 @@ function Post({ username, content, date, picture, profilePicture }) {
   return (
     <div className="post border-solid border-2 border-black m-3 p-3 w-full h-auto flex flex-col items-center">
       <div className="post-header">
-        <img src={profilePicture} alt="" />
+        <img className="rounded-full w-12 h-12" src={profilePicture} alt="" />
         <div className="post-user">{username}</div>
         <div className="post-date">{date}</div>
       </div>
