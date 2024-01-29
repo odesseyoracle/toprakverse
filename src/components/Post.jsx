4b93d3e81d698b11f28f47034c2e98b6820372a5
Jan <j.c.scherch@gmail.com>
2024-01-29 14:22:36 +0100

    bosshadow added

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function Post({ username, content, date, picture, profilePicture }) {
   return (
-    <div className="post border-solid border-2 border-black m-3 p-3 w-full h-auto flex flex-col items-center">
+    <div className="post m-3 p-3 w-auto h-auto flex flex-col items-center shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]">
       <div className="post-header flex flex-row w-full items-center justify-between">
         <div className="flex items-center ">
-        <img className="rounded-full h-12 w-12" src={profilePicture} alt="" />
-        <div className="post-user font-bold m-1">{username}</div>
+          <img className="rounded-full h-12 w-12" src={profilePicture} alt="" />
+          <div className="post-user font-bold m-1">{username}</div>
         </div>
         <div className="post-date">{date}</div>
       </div>
